01ebfa92c2d6b06d8bb0ff023a10c033639d44ac
matyb08 <suricate66@protonmail.com>
2022-06-02 17:24:38 +0200

    suppressed the output of some commands

--- mount-phone.sh
+++ mount-phone.sh
@@ -12,6 +12,6 @@ fi
 read -p "Connect the phone, hit ENTER: " enter
 mtp-detect # Triggers phone to show wheather you allow access to phone data
 read -p "Press ALLOW on phone to allow file access, hit ENTER: " enter
-sudo umount /mnt/phone/
-sudo pkill kiod5
+sudo umount /mnt/phone/ > /dev/null 2>&1
+sudo pkill kiod5 > /dev/null 2>&1
 sudo jmtpfs -o allow_other /mnt/phone/

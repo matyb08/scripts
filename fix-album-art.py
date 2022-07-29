36ed876a6a021456e35c6d1b765fc90620b88caa
matyb08 <suricate66@protonmail.com>
2022-07-29 15:05:20 +0200

    added and implemented a script that crops album art in mp3 files to correct size

---
+++ fix-album-art.py
@@ -0,0 +1,53 @@
+#!/bin/python3
+
+# Script for fixing album art when downloading music from YouTube.
+# Script removes left & right colored padding, thus making album art image squared.
+
+import os
+import sys
+from PIL import Image
+
+DIR_PATH = sys.argv[1]
+
+for file_name in os.listdir(DIR_PATH):
+    if file_name.startswith('+') and file_name.endswith('.mp3'):
+        # Extract the image from mp3 file
+        os.system(
+            'ffmpeg -i "'
+            + os.path.join(DIR_PATH, file_name)
+            + '" -an -c:v copy "'
+            + os.path.join(DIR_PATH, 'tmp-art-uncropped.jpg"')
+        )
+
+        # Crop & save the image
+        im = Image.open(os.path.join(DIR_PATH, 'tmp-art-uncropped.jpg'))
+        width, height = im.size
+
+        left = int((width - height) / 2)
+        upper = 0
+        right = left + height
+        lower = height
+
+        im = im.crop((left, upper, right, lower))
+        im.save(os.path.join(DIR_PATH, 'tmp-art-cropped.jpg'))
+
+        # Embed the image
+        os.system(
+            'ffmpeg -i "'
+            + os.path.join(DIR_PATH, file_name)
+            + '" -i "'
+            + os.path.join(DIR_PATH, 'tmp-art-cropped.jpg')
+            + '" -map 0:0 -map 1:0 -c copy -id3v2_version 3 '
+            + '-metadata:s:v title="Album cover" -metadata:s:v '
+            + 'comment="Cover (front)" "'
+            + os.path.join(DIR_PATH, file_name[1:])
+            + '"'
+        )
+
+        # Clean up
+        os.remove(os.path.join(DIR_PATH, 'tmp-art-uncropped.jpg'))
+        os.remove(os.path.join(DIR_PATH, 'tmp-art-cropped.jpg'))
+        os.remove(os.path.join(DIR_PATH, file_name))
+
+
+print('Done.')
--- update-youtube-playlists.sh
+++ update-youtube-playlists.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 # Update my YouTube music playlists
+# ALL OF THE ASSOCCIATED SCRIPTS (fix-album-art.py) OR THE WHOLE SCRIPTS FOLDER (even better), MUST BE ADDED TO PATH
 
 # 'The Playlist'
 THE_PLAYLIST_PLAYLIST=$(echo $THE_PLAYLIST_PLAYLIST_YOUTUBE)
 THE_PLAYLIST_DIR_PATH="/main-disk/Goodies/Music/The Playlist/"
 
-# 'Not Current Pop'
+# 'Not Current Pop' (OG, i know)
 NOT_CURRENT_POP_PLAYLIST=$(echo $NOT_CURRENT_POP_PLAYLIST_YOUTUBE)
 NOT_CURRENT_POP_DIR_PATH="/main-disk/Goodies/Music/Not Current Pop/"
 
@@ -18,13 +19,19 @@ DOWNLOAD_ARCHIVE_PATH="/main-disk/Goodies/Music/.playlists-downloaded.txt"
 
 # UNCOMMENT FOR TESTING
 
-# THE_PLAYLIST_DIR_PATH="/test/The Playlist/"
+# THE_PLAYLIST_DIR_PATH="/home/mathicc/test/second-final-test/"
 # NOT_CURRENT_POP_DIR_PATH="/test/Not Current Pop/"
 # KPOP_DIR_PATH="/test/kpop/"
-# DOWNLOAD_ARCHIVE_PATH="/test/.playlists-downloaded.txt"
+# DOWNLOAD_ARCHIVE_PATH="/home/mathicc/test/second-final-test/.playlists-downloaded.txt"
 
 # --
 
-yt-dlp --extract-audio --audio-quality 0 --audio-format "mp3" --ignore-errors --add-metadata --download-archive "$DOWNLOAD_ARCHIVE_PATH" --no-post-overwrites --embed-thumbnail --output "$THE_PLAYLIST_DIR_PATH""%(title)s %(id)s.%(ext)s" "$THE_PLAYLIST_PLAYLIST"
-yt-dlp --extract-audio --audio-quality 0 --audio-format "mp3" --ignore-errors --add-metadata --download-archive "$DOWNLOAD_ARCHIVE_PATH" --no-post-overwrites --embed-thumbnail --output "$NOT_CURRENT_POP_DIR_PATH""%(title)s %(id)s.%(ext)s" "$NOT_CURRENT_POP_PLAYLIST"
-yt-dlp --extract-audio --audio-quality 0 --audio-format "mp3" --ignore-errors --add-metadata --download-archive "$DOWNLOAD_ARCHIVE_PATH" --no-post-overwrites --embed-thumbnail --output "$KPOP_DIR_PATH""%(title)s %(id)s.%(ext)s" "$KPOP_PLAYLIST"
+# Downloading
+yt-dlp --extract-audio --audio-quality 0 --audio-format "mp3" --ignore-errors --add-metadata --download-archive "$DOWNLOAD_ARCHIVE_PATH" --no-post-overwrites --embed-thumbnail --output "$THE_PLAYLIST_DIR_PATH""+%(title)s %(id)s.%(ext)s" "$THE_PLAYLIST_PLAYLIST"
+yt-dlp --extract-audio --audio-quality 0 --audio-format "mp3" --ignore-errors --add-metadata --download-archive "$DOWNLOAD_ARCHIVE_PATH" --no-post-overwrites --embed-thumbnail --output "$NOT_CURRENT_POP_DIR_PATH""+%(title)s %(id)s.%(ext)s" "$NOT_CURRENT_POP_PLAYLIST"
+yt-dlp --extract-audio --audio-quality 0 --audio-format "mp3" --ignore-errors --add-metadata --download-archive "$DOWNLOAD_ARCHIVE_PATH" --no-post-overwrites --embed-thumbnail --output "$KPOP_DIR_PATH""+%(title)s %(id)s.%(ext)s" "$KPOP_PLAYLIST"
+
+# Fixing album art
+fix-album-art.py $THE_PLAYLIST_DIR_PATH
+fix-album-art.py $NOT_CURRENT_POP_DIR_PATH
+fix-album-art.py $KPOP_DIR_PATH

bc3512c4dbf092a90d211f0abce361b092122085
maticb <matic.berkopec@mikrografija.si>
2023-12-28 14:30:51 +0100

    added support for total commander (now prefered option when opening folders)

--- f.sh
+++ f.sh
@@ -1,27 +1,35 @@
 #!/usr/bin/env bash
 
-# Open files or folders from command line with default apps.
+# Open files or folders from command line with default apps or (preferably) with Total Commander, in case of folders.
 # Works with native Linux and WSL.
 
-# REQUIREMENTS: /
+# REQUIREMENTS: total commander (added to PATH)
 
-if [ $# -eq 0 ]; then # '$#' gives the number of input arguments the script was passed
-	if grep -qi microsoft /proc/version; then
-		powershell.exe -c "start '$(wslpath -aw ".")'"
-		exit 0
-	else
-		xdg-open . > /dev/null 2>&1 &
-		exit 0
-	fi
-
-else
-	if grep -qi microsoft /proc/version; then
-		for path in "$@"; do
-			powershell.exe -c "start '$(wslpath -aw "$path")'"
-		done
-                exit 0
+if [ $# -eq 0 ]; then # at least one path is given
+    if grep -qi microsoft /proc/version; then
+        if command -v totalcmd.exe >/dev/null 2>&1; then
+            powershell.exe -c "totalcmd.exe /O /T /L=\"$(wslpath -aw ".")\""
         else
-		xdg-open $@ > /dev/null 2>&1 &
-		exit 0
-	fi
+            powershell.exe -c "start '$(wslpath -aw ".")'"
+        fi
+        exit 0
+    else
+        xdg-open . > /dev/null 2>&1 &
+        exit 0
+    fi
+
+else # no path is given
+    if grep -qi microsoft /proc/version; then
+        for path in "$@"; do
+            if [ -d "$path" ] && command -v totalcmd.exe >/dev/null 2>&1; then
+                powershell.exe -c "totalcmd.exe /O /T /L=\"$(wslpath -aw "$path")\""
+            else
+                powershell.exe -c "start '$(wslpath -aw "$path")'"
+            fi
+        done
+        exit 0
+    else
+        xdg-open $@ > /dev/null 2>&1 &
+        exit 0
+    fi
 fi

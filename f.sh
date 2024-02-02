12d3a3fc299f6ccf05a31d126cb118442e78820b
matyb08 <suricate66@protonmail.com>
2024-02-02 19:09:29 +0100

    fixed opening multiple paths on native linux refactored code

--- f.sh
+++ f.sh
@@ -5,20 +5,24 @@
 
 # REQUIREMENTS: total commander (added to PATH)
 
-if [ $# -eq 0 ]; then # at least one path is given
+# no path is given
+if [ $# -eq 0 ]; then
+    # wsl
     if grep -qi microsoft /proc/version; then
         if command -v totalcmd64.exe >/dev/null 2>&1; then
             powershell.exe -c "totalcmd64.exe /O /T /L=\"$(wslpath -aw ".")\""
         else
             powershell.exe -c "start '$(wslpath -aw ".")'"
         fi
-        exit 0
+  
+    # native
     else
         xdg-open . > /dev/null 2>&1 &
-        exit 0
     fi
 
-else # no path is given
+# one or more paths are given
+else
+    # wsl
     if grep -qi microsoft /proc/version; then
         for path in "$@"; do
             if [ -d "$path" ] && command -v totalcmd64.exe >/dev/null 2>&1; then
@@ -27,9 +31,11 @@ else # no path is given
                 powershell.exe -c "start '$(wslpath -aw "$path")'"
             fi
         done
-        exit 0
+  
+    # native
     else
-        xdg-open $@ > /dev/null 2>&1 &
-        exit 0
+        for path in "$@"; do
+            xdg-open "$path" > /dev/null 2>&1 &
+        done
     fi
 fi
